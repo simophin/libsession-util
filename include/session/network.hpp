df9438d6f5c31167d30fe1b3deb5cca5ea0516df
Morgan Pretty <morgan.t.pretty@gmail.com>
2024-03-15 10:19:12 +1100

    Fixed an incorrect function definition

--- include/session/network.hpp
+++ include/session/network.hpp
@@ -10,7 +10,7 @@ void send_request(
         ustring_view ed_sk,
         oxen::quic::RemoteAddress target,
         std::string endpoint,
-        ustring body,
+        std::optional<ustring> body,
         std::function<void(bool success, int16_t status_code, std::optional<std::string> response)>
                 handle_response);
 
