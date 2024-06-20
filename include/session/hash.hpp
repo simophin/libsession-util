926d05f7e0682661b30b073790f03440bbf2c207
Jason Rhinelander <jason@imaginary.ca>
2024-06-19 22:26:59 -0300

    Clean up hash function
    - don't worry about checking the return value; blake2b hashing always
      returns 0.
    - Change VLA usage to a single ustring allocation that we write into
      directly (this is likely what MSVC was unhappy with).
    - De-duplicate the `crypto_generichash_blake2b` call (by using a ternary
      on the differing key arguments).
    - Removed some no-op `static_cast`s

--- include/session/hash.hpp
+++ include/session/hash.hpp
@@ -13,10 +13,11 @@ namespace session::hash {
 /// Inputs:
 /// - `size` -- length of the hash to be generated.
 /// - `msg` -- the message to generate a hash for.
-/// - `key` -- an optional key to be used when generating the hash.
+/// - `key` -- an optional key to be used when generating the hash.  Can be omitted or an empty
+///   string for an unkeyed hash.
 ///
 /// Outputs:
 /// - a `size` byte hash.
-ustring hash(const size_t size, ustring_view msg, std::optional<ustring_view> key);
+ustring hash(const size_t size, ustring_view msg, std::optional<ustring_view> key = std::nullopt);
 
 }  // namespace session::hash
