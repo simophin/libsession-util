9517c2184b098ad850ba9dba9b203efb900e5829
Morgan Pretty <morgan.t.pretty@gmail.com>
2024-08-16 12:39:40 +1000

    Further tweaks

--- Package.swift
+++ Package.swift
@@ -5,18 +5,29 @@ import PackageDescription
 
 let package = Package(
     name: "libSession-util",
+    defaultLocalization: "en",
     platforms: [
         .iOS(.v12)
     ],
     products: [
-        .library(name: "SessionUtil", targets: ["libSession-util"]),
+        .library(name: "SessionUtil", targets: ["libSession-util-packaged"]),
+        .library(name: "SessionUtilSource", targets: ["libSession-util-source"]),
     ],
     targets: [
         .binaryTarget(
             name: "libSession-util-packaged",
             url: "https://github.com/mpretty-cyro/libsession-util/releases/download/0.0.1/libsession-util.xcframework.zip",
-            checksum: "3c50abe18b6bd5edfe42b9cef531ea6778c8854c0e67bbe3d3d630f8bb1441e5"
+            checksum: "839d6a6a46ad7646ffb75914f74a911dfd7c9dedc391db5f15c18126f203b01a"
         ),
-        .target(name: "libSession-util-source")
+        .target(
+            name: "libSession-util-source",
+            path: ".",
+            sources: [
+                "src",
+                "proto",
+                "utils",
+            ],
+            publicHeadersPath: "include"
+        )
     ]
 )
